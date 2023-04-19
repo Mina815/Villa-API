3dae370fe095361a2179c46e53767d4e5c2126c7
Mina Latif <mina.mona815@gmail.com>
2023-04-19 20:46:43 +0200

    First EndPoint - HttpGet

---
+++ Villa-API/Controllers/VillaAPIController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa_API.Models;
+
+namespace Villa_API.Controllers
+{
+	[Route("Api/VillaAPI")]
+	[ApiController]
+	public class VillaAPIController : ControllerBase
+	{
+		[HttpGet]
+		public IEnumerable<Villa> VillaList()
+		{
+			return new List<Villa>() {
+				new Villa { Id = 1, Name = "Villa1" },
+				new Villa { Id = 2, Name = "Villa2" },
+			};
+		}
+	}
+}
