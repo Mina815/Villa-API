1ca8ccb7a4629e912f9e71c349bd23248c371f61
Mina Latif <mina.mona815@gmail.com>
2023-04-21 17:20:21 +0200

    HttpPost With Validation

--- Villa-API/Controllers/VillaAPIController.cs
+++ Villa-API/Controllers/VillaAPIController.cs
@@ -7,6 +7,7 @@ using Villa_API.Models.Dto;
 namespace Villa_API.Controllers
 {
 	[Route("Api/VillaAPI")]
+	// If you remove apiController annotation you have to exciplicitly validate the model by using model state
 	[ApiController]
 	public class VillaAPIController : ControllerBase
 	{
@@ -16,13 +17,32 @@ namespace Villa_API.Controllers
 			return Ok(VillaStore.VillaList);
 		}
 
-		[HttpGet("{id:int}")]
+		[HttpGet("{id:int}", Name = "GetVilla")]
+		// For Document the response types 
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+
 		public ActionResult<VillaDTO> Getvilla(int id)
 		{
 			if(id == 0) return BadRequest();
 			var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
 			if(villa == null) return NotFound();
 			return villa;
+		}
+		[HttpPost]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		public ActionResult<VillaDTO> CreateVilla(VillaDTO Villa)
+		{
+			//if(!ModelState.IsValid) return BadRequest(Villa);
+			if(Villa == null) return BadRequest(Villa);
+			if(Villa.Id > 0) return StatusCode(StatusCodes.Status500InternalServerError);
+
+			Villa.Id = VillaStore.VillaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+			VillaStore.VillaList.Add(Villa);
+			return CreatedAtRoute("GetVilla",new {id = Villa.Id},Villa);
 		}
 	}
 }
